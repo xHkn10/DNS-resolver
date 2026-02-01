7101cd9f8e40b0db52bfc7e5b1b9f9db61557a01
Hakan Akbiyik <hakanakbiyik@192.168.1.38>
2026-02-01 23:39:00 +0300

    better tcp error handling

--- src/AsyncTcpServer.cpp
+++ src/AsyncTcpServer.cpp
@@ -10,6 +10,7 @@
 #include <boost/asio/use_awaitable.hpp>
 #include <boost/asio/detached.hpp>
 #include <array>
+#include <sys/socket.h>
 #include <vector>
 
 #ifndef DISABLE_STATISTICS
@@ -29,7 +30,14 @@ AsyncTcpServer::listen(u16 port) {
     tcp::acceptor acceptor{executor, {tcp::v4(), port}};
 
     while (true) {
-        tcp::socket sock = co_await acceptor.async_accept(use_awaitable);
+        boost::system::error_code ec;
+        
+        tcp::socket sock = co_await acceptor.async_accept(
+            net::redirect_error(use_awaitable, ec)
+        );
+        if (ec)
+            continue;
+
         net::co_spawn(
             executor,
             handle_cli(std::move(sock)),
@@ -48,11 +56,16 @@ AsyncTcpServer::send_dns_bytes(
     std::array<u8, 2> dns_len_buf = {
         static_cast<u8>(sz >> 8), static_cast<u8>(sz & 0xFF)
     };
+
+    boost::system::error_code ec;
+
     co_await net::async_write(
-        sock, net::buffer(dns_len_buf), use_awaitable
+        sock, net::buffer(dns_len_buf),
+        net::redirect_error(use_awaitable, ec)
     );
     co_await net::async_write(
-        sock, net::buffer(bytes), use_awaitable
+        sock, net::buffer(bytes),
+        net::redirect_error(use_awaitable, ec)
     );
 }
 
@@ -61,11 +74,32 @@ AsyncTcpServer::rcv_dns_bytes(
     tcp::socket& sock,
     std::vector<u8>& buf
 ) {
+
     std::array<u8, 2> dns_len_buf;
-    co_await net::async_read(sock, net::buffer(dns_len_buf), use_awaitable);
+
+    {
+        boost::system::error_code ec;
+        co_await net::async_read(
+            sock, net::buffer(dns_len_buf),
+            net::redirect_error(use_awaitable, ec)
+        );
+        if (ec)
+            co_return 0;
+    }
+
     size_t len = (dns_len_buf[0] << 8) | dns_len_buf[1];
     buf.resize(len);
-    co_await net::async_read(sock, net::buffer(buf), use_awaitable);
+    
+    {
+        boost::system::error_code ec;
+        co_await net::async_read(
+            sock, net::buffer(buf),
+            net::redirect_error(use_awaitable, ec)
+        );
+        if (ec)
+            co_return 0;
+    }
+
     co_return len;
 }
 
@@ -76,53 +110,53 @@ AsyncTcpServer::handle_cli(tcp::socket sock) {
     ScopedMeasure measure{Metric::cli_resolve_total_tcp};
     #endif
     
-    try {
-        while (true) {
-            std::vector<u8> cli_query_bytes(4096);
-            co_await rcv_dns_bytes(sock, cli_query_bytes);
-        
-            if (cli_query_bytes.size() < Header::HEADER_SZ)
-                co_return;
-        
-            auto cli_query_msg = Message::deserialize(cli_query_bytes);
-            if (!cli_query_msg) {
-                Message::make_formerr(cli_query_bytes);
-                co_await send_dns_bytes(sock, cli_query_bytes);
-                co_return;
-            }
-        
-            if (cli_query_msg->questions.size() == 0)
-                co_return;
-            if (!cli_query_msg->header.is_rd()) {
-                Message::make_servfail(cli_query_bytes);
-                co_await send_dns_bytes(sock, cli_query_bytes);
-                co_return;
-            }
-        
-            ResolverResult res = co_await resolver.resolve(cli_query_msg->questions.front());
-            if (res.status != ResolverStatus::Success) {
-                Message::make_servfail(cli_query_bytes);
-                co_await send_dns_bytes(sock, cli_query_bytes);
-                co_return;
-            }
-        
-            ClientContext cli_ctx{
-                .id = cli_query_msg->header.id,
-                .cli_q = cli_query_msg->questions.front()
-            };
-            cli_query_msg->assign_edns_related_fields(cli_ctx);
-        
-            Message::finalize_response(res, cli_ctx);
-            auto response_bytes = res.msg.serialize();
-        
-            if (!response_bytes)
-                co_return;
-        
-            co_await send_dns_bytes(sock, *response_bytes);
+    std::vector<u8> cli_query_bytes(4096);
+    co_await rcv_dns_bytes(sock, cli_query_bytes);
 
-            LOG("response sent\n");
+    if (cli_query_bytes.size() < Header::HEADER_SZ)
+        goto shutdown;
+
+    {
+        auto cli_query_msg = Message::deserialize(cli_query_bytes);
+        if (!cli_query_msg) {
+            Message::make_formerr(cli_query_bytes);
+            co_await send_dns_bytes(sock, cli_query_bytes);
+            goto shutdown;
         }
-    } catch (const boost::system::system_error& e) {
-        co_return;
+        if (cli_query_msg->questions.size() == 0)
+            goto shutdown;
+        if (!cli_query_msg->header.is_rd()) {
+            Message::make_servfail(cli_query_bytes);
+            co_await send_dns_bytes(sock, cli_query_bytes);
+            goto shutdown;
+        }
+
+        ResolverResult res = co_await resolver.resolve(cli_query_msg->questions.front());
+        if (res.status != ResolverStatus::Success) {
+            Message::make_servfail(cli_query_bytes);
+            co_await send_dns_bytes(sock, cli_query_bytes);
+            goto shutdown;
+        }
+
+        ClientContext cli_ctx{
+            .id = cli_query_msg->header.id,
+            .cli_q = cli_query_msg->questions.front()
+        };
+        cli_query_msg->assign_edns_related_fields(cli_ctx);
+    
+        Message::finalize_response(res, cli_ctx);
+        auto response_bytes = res.msg.serialize();
+
+        if (!response_bytes)
+            goto shutdown;
+
+        co_await send_dns_bytes(sock, *response_bytes);
     }
+
+    shutdown:
+    boost::system::error_code ec;
+    ec = sock.shutdown(tcp::socket::shutdown_send, ec);
+    sock.close();
+
+    LOG("Connection shutdown\n");
 }
